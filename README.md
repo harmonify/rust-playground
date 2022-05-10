4f1ed1e433f1f3d0714210bfb417e2d7ec3e61a1
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-05-10 14:05:34 +0000

    add more documentations

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Introduction
 
-`rust-playground` is a playground for Rust.
+`rust-playground`, a playground for Rust.
+
+## Setup
+
+You can easily install the Rust environment with VSCode and Docker devcontainer by opening the `rust-playground` folder in the VSCode editor.
--- build.sh
+++ build.sh
@@ -4,4 +4,6 @@
 cargo build --release
 
 # target windows
+# assuming one run the environment from Unix and targetting Windows
+# https://stackoverflow.com/questions/31492799/cross-compile-a-rust-application-from-linux-to-windows
 cargo build --release --target x86_64-pc-windows-gnu
