da84a799a2e5478ffeece460832e1fbb96cf8d9d
Wendy Surya Wijaya <84518752+harmonify@users.noreply.github.com>
2022-05-10 13:59:47 +0000

    more content on variables

--- src/variables/primitive_types.rs
+++ src/variables/primitive_types.rs
@@ -3,7 +3,7 @@
 /// Variables hold primitive data or references to data
 /// Variables are immutable by default
 /// Rust is a block-scoped language
-/// 
+///
 /// ## Integers
 ///
 /// i8, i16, i32, i64, i128, u8, u16, u32, u64, u128
@@ -14,7 +14,7 @@
 /// ## Floating-point numbers
 ///
 /// f32, f64
-/// 
+///
 /// By default, the floating-point type is f64.
 ///
 /// ## Boolean
@@ -29,24 +29,26 @@
 pub fn run() {
     let name = "John"; // by default, immutable
     let mut age = 30; // mutable
-    let bank_balance = 1000.00;
-    
     age = 31;
-    println!("{name} is {age} years old and has ${bank_balance} in his bank account\n");
-    
+    let bank_balance = 1000.00;
+    let emoji = '\u{1F600}';
+
+    println!("{name} is {age} years old and has ${bank_balance} in his bank account {emoji}\n");
+
     print_primitive_types_size();
 }
 
 fn print_primitive_types_size() {
     println!("\nPrimitive types sizes:");
 
-    println!("Integer 32 size: {:?}", std::mem::size_of::<i32>());
-    println!("Integer 64 size: {:?}", std::mem::size_of::<i64>());
-    println!("Unsigned integer 32 size: {:?}", std::mem::size_of::<u32>());
+    println!("i32 min: {}; i32 max: {}; i32 size: {}", i32::MIN, i32::MAX, std::mem::size_of::<i32>());
+    println!("i64 min: {}; i64 max: {}; i64 size: {}", i64::MIN, i64::MAX, std::mem::size_of::<i64>());
+    println!("u32 min: {}; u32 max: {}; u32 size: {}", u32::MIN, u32::MAX, std::mem::size_of::<u32>());
 
-    println!("Float 64 size: {:?}", std::mem::size_of::<f64>());
+    println!("f64 min: {}; f64 max: {}; f64 size: {}", f64::MIN, f64::MAX, std::mem::size_of::<f64>());
 
-    println!("boolean size: {:?}", std::mem::size_of::<bool>());
+    println!("bool size: {:?}", std::mem::size_of::<bool>());
     println!("char size: {:?}", std::mem::size_of::<char>());
-    println!("string size: {:?}", std::mem::size_of::<&str>());
+    println!("&str size: {:?}", std::mem::size_of::<&str>());
+    println!();
 }
